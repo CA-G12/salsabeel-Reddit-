4dcd34619cd1e2e168c18e27de3f070cbbfc4eba
salsabeelomar <omarsalsabeel65@gmail.com>
2022-09-07 01:38:36 +0300

    fix th path  relates #13

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(authorization);
 
 app.get('/', (req, res) => {
-  if (req.user) res.sendFile(join(__dirname, '..', 'public'));
+  if (req.user) res.sendFile(join(__dirname, '..', 'public','index.html'));
   else { res.sendFile(join(__dirname, '..', 'public', 'login'))}
 });
 app.use(express.static(join(__dirname, '..', 'public','login')))
